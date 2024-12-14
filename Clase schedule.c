b6d516156896b82a9b41171683718c7fbc00b39b
EnriqueBDL <131392738+EnriqueBDeL@users.noreply.github.com>
2024-12-14 18:48:18 +0100

    Update Clase schedule.c

--- Clase schedule.c
+++ Clase schedule.c
@@ -10,7 +10,7 @@ int main(int argc, char **argv) {
     printf("\nEstatico:\n");
 
     #pragma omp parallel for schedule(static, 2)    //schedule(estrategia de iteracion, tamaño de bloque de iteracion asignado a cada hilo)
-    // Los bloques se asignan a los bloques en orden. 
+    // Los bloques se asignan a los hilos en orden. 
     // Ej de este caso: bloque[i = 0 y i = 1] es ejecutado por el hilo 0.
     for (int i = 0; i < 10; i++) {
         printf("Iteracion %d ejecutada por el hilo %d.\n", i, omp_get_thread_num());
